30500a830daa4436379e9ab905d31af1ddcda4ed
Raunak Pandey <raunakpandey1007@gmail.com>
2022-08-04 14:35:07 +0530

    make it better

--- configuration/db.js
+++ configuration/db.js
@@ -2,10 +2,9 @@ const mongoose = require("mongoose");
 require('dotenv').config({path:"./config.env"})
 const connectDB = async () => {
   await mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: true,
+            useUnifiedTopology:true,
+            useNewUrlParser: true,
+            useCreateIndex: true
   });
 
   console.log("MongoDB Connected");
