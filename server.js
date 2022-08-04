9a35450fc91f6448ffe030f297f02fb4c03b0eac
Raunak Pandey <raunakpandey1007@gmail.com>
2022-08-04 14:42:43 +0530

    make it better

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-require('dotenv').config({path: "./config.env"})
+require('dotenv').config({path: "config.env"})
 const app = express();
 const connectDB = require("./configuration/db");
 const errorHandler = require("./middleware/error");
