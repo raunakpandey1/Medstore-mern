98cce6648e14a475a89a430a3bf8a6d8680edd23
Raunak Pandey <raunakpandey1007@gmail.com>
2023-02-04 03:57:21 +0530

    dep1

--- server.js
+++ server.js
@@ -4,14 +4,16 @@ const app = express();
 
 const connectDB = require("./configuration/db");
 const errorHandler = require("./middleware/error");
-
+const path = require("path");
 connectDB();
 
+app.use(express.static(path.join(process.cwd(), 'public')))
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }))
 
-/* app.get("/", (req, res, next) => {
+  app.get("/", (req, res, next) => {
   res.send("Api running");
-}); */
+});  
 
 // Connecting Routes
 app.use("/api/auth", require("./routes/auth"));
@@ -23,13 +25,17 @@ app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
-if (process.env.NODE_ENV == "production"){
-  app.use(express.static("client/build"));
-  const path = require("path");
-  app.get("*", (req, res) => {
-      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  })
-}
+// if (process.env.NODE_ENV == "production"){
+//   app.use(express.static("client/build"));
+//   const path = require("path");
+//   app.get("*", (req, res) => {
+//       res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+//   })
+// }
+
+app.use(function (req, res, next) {
+  res.sendFile(path.join(process.cwd(), 'public', 'index.html'))
+})
 
 /* const path = require("path");
   app.get("*", (req, res) => {
